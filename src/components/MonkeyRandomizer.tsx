705ee4a0da2e5a4fc0acae74ffc6b9b514ed1dd4
Samuel White <samuelhwhite01@gmail.com>
2025-05-20 01:19:34 -0700

    Added checking for towers that can provide other towers with lead/camo

--- src/components/MonkeyRandomizer.tsx
+++ src/components/MonkeyRandomizer.tsx
@@ -4,22 +4,24 @@ import MonkeyType from '../types/MonkeyType';
 import { useEffect, useMemo, useState } from 'react';
 
 const MonkeyRandomizer: React.FC = () => {
-  const {map} = useMapContext();
-  const [selectedMonkeys, setSelectedMonkeys] = useState<MonkeyType[]>([])
-  const [numRandom, setNumRandom] = useState(3);
-  const images = import.meta.glob('../assets/Monkey_Images/*.webp', { eager: true, as: 'url' });
-  const numMonkeys = useMemo(() => {
-  return map.hasWater ? monkeyData.length  : monkeyData.length-2;
-}, [map]);
-  const maxNumRandom = 7; // sets the maximum number of monkeys to randomize
-  function getRandomIndices(): number[] {
-    const indices = Array.from({ length: numMonkeys }, (_, i) => i);
-    for (let i = indices.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [indices[i], indices[j]] = [indices[j], indices[i]]; // Shuffle
+    const globalLeadLookup = ["Alchemist"]
+    const globalCamoLookup = ["Wizard", "Mortar", "Ninja", "Sub", "Village", "Engineer", "Ice", "Mermonkey"]
+    const {map} = useMapContext();
+    const [selectedMonkeys, setSelectedMonkeys] = useState<MonkeyType[]>([])
+    const [numRandom, setNumRandom] = useState(3);
+    const images = import.meta.glob('../assets/Monkey_Images/*.webp', { eager: true, as: 'url' });
+    const numMonkeys = useMemo(() => {
+    return map.hasWater ? monkeyData.length  : monkeyData.length-2;
+    }, [map]);
+    const maxNumRandom = 7; // sets the maximum number of monkeys to randomize
+    function getRandomIndices(): number[] {
+        const indices = Array.from({ length: numMonkeys }, (_, i) => i);
+        for (let i = indices.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [indices[i], indices[j]] = [indices[j], indices[i]]; // Shuffle
+        }
+        return indices.slice(0, numRandom);
     }
-    return indices.slice(0, numRandom);
-  }
 
   useEffect(() => {
     handleRandomize()
@@ -27,44 +29,51 @@ const MonkeyRandomizer: React.FC = () => {
     
   const monkeyCheck = (arr:MonkeyType[]) =>
   {
-    let leadCheck = false
-    let camoCheck = false
     let camoLeadCheck = false
+    let globalLead = false
+    let globalCamo = false
     for (let i = 0; i<arr.length; i++)
     {
-
-      let localCamo = false
-      let localLead = false
-      if (arr[i].lead[0][0] > -1) // will be -1 if its never true
-      {
-        leadCheck = true
-        localLead = true
-      }
-      if (arr[i].camo[0][0] > -1) // will be -1 if its never true
-      {
-        camoCheck = true
-        localCamo = true
-      }
-      // in the case that both lead and camo are true, then loop over the possibilites to see if theyre compatible
-      if(localLead && localCamo)
-      {
-        for(let j = 0; j<arr[i].lead.length; j++)
+        let localCamo = false
+        let localLead = false
+        if(globalCamoLookup.includes(arr[i].name))
         {
-          if (arr[i].lead[j][1] < 3)
-          {
-            camoLeadCheck = true
-          }
+            console.log(arr[i].name,"provides global camo")
+            globalCamo = true;
         }
-        for(let j = 0; j<arr[i].camo.length; j++)
+        if(globalLeadLookup.includes(arr[i].name))
         {
-          if (arr[i].lead[j][1] < 3)
-          {
-            camoLeadCheck = true
-          }
+            console.log(arr[i].name,"provides global lead")
+            globalLead = true;
         }
-      }
+        if (arr[i].lead[0][0] > -1) // will be -1 if its never true
+        {
+            localLead = true
+        }
+        if (arr[i].camo[0][0] > -1) // will be -1 if its never true
+        {
+            localCamo = true
+        }
+        // in the case that both lead and camo are true, then loop over the possibilites to see if theyre compatible
+        if((localLead||globalLead) && (localCamo||globalCamo))
+        {
+            for(let j = 0; j<arr[i].lead.length; j++)
+            {
+            if (arr[i].lead[j][1] < 3)
+            {
+                camoLeadCheck = true
+            }
+            }
+            for(let j = 0; j<arr[i].camo.length; j++)
+            {
+            if (arr[i].lead[j][1] < 3)
+            {
+                camoLeadCheck = true
+            }
+            }
+        }
     }
-    if(camoCheck && leadCheck && camoLeadCheck)
+    if(camoLeadCheck)
     {
       return true
     }
